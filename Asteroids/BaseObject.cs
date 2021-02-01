43baa7314f2f9b172a058c35c57dc97d42f5de30
Daniel <pokinuto@bk.ru>
2021-02-01 15:31:17 +0400

    All tasks completed;

--- Asteroids/BaseObject.cs
+++ Asteroids/BaseObject.cs
@@ -7,11 +7,20 @@ using System.Threading.Tasks;
 
 namespace Asteroids
 {
-    class BaseObject
+    class GameObjectException : ApplicationException
     {
-        protected Point Pos { get; set; }
-        protected Point Dir { get; set; }
-        protected Size Size { get; set; }
+        public GameObjectException(string message) : base(message)
+        {
+
+        }
+    }
+    abstract class BaseObject : ICollision
+    {
+        public Point Pos { get; set; }
+        public Point Dir { get; set; }
+        public Size Size { get; set; }
+
+        public Rectangle Rect => new Rectangle(Pos, Size);
 
         public BaseObject()
         {
@@ -22,27 +31,21 @@ namespace Asteroids
 
         public BaseObject(Point pos, Point dir, Size size)
         {
+            if (size.Width < 0 || size.Height < 0)
+                 throw new GameObjectException("Size cannot be defined by negative values");
+            if (dir.X > 100 || dir.X < -100 || dir.Y > 100 || dir.Y < -100)
+                throw new GameObjectException("Direction can be defined within -100 to 100");
             Pos = pos;
             Dir = dir;
             Size = size;
         }
 
-        public virtual void Draw()
+        public abstract void Draw();
+        public abstract void Update();
+
+        public bool Collision(ICollision obj)
         {
-            Game.Buffer.Graphics.DrawImage(Image.FromFile("Images\\BaseObjectDefaultImage.png"),
-                Pos.X, Pos.Y, Size.Height, Size.Width);
-        }
-        public virtual void Update()
-        {
-            Pos = new Point(Pos.X + Dir.X, Pos.Y + Dir.Y);
-            if (Pos.X > Game.Width)
-                Pos = new Point(0, Pos.Y);
-            if (Pos.X < 0)
-                Pos = new Point(Game.Width, Pos.Y);
-            if (Pos.Y > Game.Height)
-                Pos = new Point(Pos.X, 0);
-            if (Pos.Y < 0)
-                Pos = new Point(Pos.X, Game.Height);
+            return this.Rect.IntersectsWith(obj.Rect);
         }
     }
     class Star : BaseObject
@@ -56,6 +59,19 @@ namespace Asteroids
         {
             Game.Buffer.Graphics.DrawImage(Image, Pos.X, Pos.Y, Size.Height, Size.Width);
         }
+
+        public override void Update()
+        {
+            Pos = new Point(Pos.X + Dir.X, Pos.Y + Dir.Y);
+            if (Pos.X > Game.Width)
+                Pos = new Point(0, Pos.Y);
+            if (Pos.X < 0)
+                Pos = new Point(Game.Width, Pos.Y);
+            if (Pos.Y > Game.Height)
+                Pos = new Point(Pos.X, 0);
+            if (Pos.Y < 0)
+                Pos = new Point(Pos.X, Game.Height);
+        }
     }
     class Planet : BaseObject
     {
@@ -67,6 +83,18 @@ namespace Asteroids
         public override void Draw()
         {
             Game.Buffer.Graphics.DrawImage(Image, Pos.X, Pos.Y, Size.Height, Size.Width);
+        }
+        public override void Update()
+        {
+            Pos = new Point(Pos.X + Dir.X, Pos.Y + Dir.Y);
+            if (Pos.X > Game.Width)
+                Pos = new Point(0 - Size.Width, Pos.Y);
+            if (Pos.X < 0)
+                Pos = new Point(Game.Width + Size.Width, Pos.Y);
+            if (Pos.Y > Game.Height)
+                Pos = new Point(Pos.X, 0 - Size.Height);
+            if (Pos.Y < 0)
+                Pos = new Point(Pos.X, Game.Height + Size.Height);
         }
     }
     class Galaxy : BaseObject
@@ -80,6 +108,61 @@ namespace Asteroids
         {
             Game.Buffer.Graphics.DrawImage(Image, Pos.X, Pos.Y, Size.Height, Size.Width);
         }
+        public override void Update()
+        {
+            Pos = new Point(Pos.X + Dir.X, Pos.Y + Dir.Y);
+            if (Pos.X > Game.Width)
+                Pos = new Point(0, Pos.Y);
+            if (Pos.X < 0)
+                Pos = new Point(Game.Width, Pos.Y);
+            if (Pos.Y > Game.Height)
+                Pos = new Point(Pos.X, 0);
+            if (Pos.Y < 0)
+                Pos = new Point(Pos.X, Game.Height);
+        }
     }
-    
+    class Asteroid : BaseObject
+    {
+        static Image Image { get; } = Image.FromFile("Images\\Asteroid.png");
+        public Asteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+
+        }
+        public Asteroid()
+        {
+
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(Image, Pos.X, Pos.Y, Size.Height, Size.Width);
+        }
+
+        public override void Update()
+        {
+            Pos = new Point(Pos.X + Dir.X, Pos.Y);
+            if (Pos.X < -Size.Width)
+                Pos = new Point(Game.Width + Game.Random.Next(1, 100),
+                    Game.Random.Next(0, Game.Height));
+        }
+    }
+    class Bullet : BaseObject
+    {
+        //static Image Image { get; } = Image.FromFile("Images\\Asteroid.png");
+        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillRectangle(Brushes.Red, Pos.X, Pos.Y, Size.Height, Size.Width);
+        }
+
+        public override void Update()
+        {
+            Pos = new Point(Pos.X + Dir.X, Pos.Y);
+            if (Pos.X < -Size.Width)
+                Pos = new Point(Game.Width + Game.Random.Next(1, 100),
+                    Game.Random.Next(0, Game.Height));
+        }
+    }
 }
---
+++ Task1(Employess)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Employees_
+{
+    class Program
+    {
+        static void EmployeesInputInConsole(Employee[] employees)
+        {
+            ArrayEmployees arrayEmployees = new ArrayEmployees(employees);
+            foreach (Employee employee in arrayEmployees)
+                Console.WriteLine("Name: {0}| Date of birth: {1}", employee.Name, employee.DateOfBirth.ToShortDateString());
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[4] 
+            {
+                new HourlyEmployee("Ben", new DateTime(1985, 01, 01)),
+                new MonthlyEmployee("Arnold", new DateTime(1985, 02, 01)),
+                new MonthlyEmployee("Dan", new DateTime(1990, 09, 04)),
+                new HourlyEmployee("Ethan", new DateTime(1983, 04, 03)) 
+            };
+            Console.WriteLine("Initial array");
+            EmployeesInputInConsole(employees);
+
+            Array.Sort(employees);
+            Console.WriteLine("Internal sort (ByDate)");
+            EmployeesInputInConsole(employees);
+
+            Array.Sort(employees, new SortByName());
+            Console.WriteLine(new SortByName().ToString());
+            EmployeesInputInConsole(employees);
+
+            Array.Sort(employees, new SortByDate());
+            Console.WriteLine(new SortByDate().ToString());
+            EmployeesInputInConsole(employees);
+
+            Console.WriteLine("Internal foreach with List collection");
+            ListEmployees listEmployees = new ListEmployees(employees);
+            foreach (Employee employee in listEmployees)
+                Console.WriteLine("Name: {0}| Date of birth: {1}", employee.Name, employee.DateOfBirth.ToShortDateString());
+
+            Console.ReadKey();
+        }
+    }
+}
